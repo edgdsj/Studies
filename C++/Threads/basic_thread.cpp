48ba4ff026a3d53379c6b2169e943e1e544d408a
Edmilson Junior <edgdsj@gmail.com>
2022-07-31 13:27:35 -0400

    Inicio

---
+++ C++/Threads/basic_thread.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <thread>
+
+void sthread(int number){
+    std::cout << "Thread Execution" << number << std::endl;
+}
+
+int main (void){
+    std::cout << "Simple thread test" << std::endl; 
+    std::cout << "Create a Thread" << std::endl;
+    for(int i = 0; i <10 ;i++){
+        std::thread th_obj(sthread, i);
+        th_obj.join();
+    }
+    return 0;
+}
