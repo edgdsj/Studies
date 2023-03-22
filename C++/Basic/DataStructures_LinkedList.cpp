8b0c7a0345b88c5780713317d4d5bd84baee14a2
Edmilson Souza Junior <edgdsj@gmail.com>
2023-03-22 11:50:19 +0100

    adding rust studies

---
+++ C++/Basic/DataStructures_LinkedList.cpp
@@ -0,0 +1,9 @@
+/*
+A linked List is a type of structure that holds a node and points to the next node.
+    It can be implmented in a struct or using class
+    I saw this being used on Sidia. Where they implement a test executor on the soundbar. 
+    The tests were writen in this kid of struct. it contained the information of the current test and pointed to the next test
+    the main advantage is that you can easily add, insert or remove an item from the list
+A second option is use double linked list, this structure points not only to the next node but also for the previuos one.
+    It brings more versatility to the list, with double list you also can insert a node before another node,
+    but it will consume more memory than the linked list*/
---
+++ C++/Basic/TypeCasting.cpp
@@ -0,0 +1,45 @@
+/*
+    Type casting is the hability to tranform the type of a variable. There are two modes of casting
+        *Implicit cast
+        *Explicit cast
+    Implicit cast ocurr every time the you assing one variable to another variable with a diferente type.
+    Explicit cast ocurr when you explicit tells to the compiler how to convert the variable
+        * static_cast
+        * dynamic_cast
+        * const cast
+        * Reinterpret cast
+        * Assigment oprator -> ()
+*/
+
+/*Static Cast. It is executed during the compile time and you have to assure that this cast is valid cast.*/
+/*Dynamic Cast. It is executed during the runtime and is used to cast pointers and references.
+ If the convertion fails it´ll return NULL
+ dynamic_cast<New type>(old type pointer)*/
+
+10
+water 64237
+frost 90429
+frost 21145
+KyVGdIuPDyAmXTYvHjvb 89174 SgogFKGgHVuBRLEFmxIoRUFmAKSwraubnxjaKanDEdLVSfepzCxnElmZqkeQbqraW
+fire 86721
+thunder 32104
+water 71342
+jJSmXUUUzvYPGzSTvpbMPKtCLUZGlzElpAAyVYHWKQmZIxfotvxjSohrCTbgDezSoRqEhYxozhE 42186 XcGcWcELLQw
+frost 304
+water 15790
+
+Waterbolt: 64237
+Frostbite: 90429
+Frostbite: 21145
+7
+Fireball: 86721
+Thunderstorm: 32104
+Waterbolt: 71342
+4
+Frostbite: 304
+Waterbolt: 15790
+
+
+
+KyVGdIuPDyAmXTYvHjvb
+SgogFK
